c5fb14f3617fd3c854c26f86bb432208489955ac
YL <110099342+YLscnu@users.noreply.github.com>
2023-03-15 17:40:23 +0800

    update the ResNetxx Model

--- models/model.py
+++ models/model.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from models.resnet50_1D import *
+from models.resnet34_1D import *
+from models.resnet18_1D import *
 from utils.masking import TriangularCausalMask, ProbMask
 from models.encoder import Encoder, EncoderLayer, ConvLayer, EncoderStack
 from models.decoder import Decoder, DecoderLayer
@@ -16,6 +19,8 @@ class Informer(nn.Module):
                 device=torch.device('cuda:0')):
         super(Informer, self).__init__()
         self.pred_len = out_len
+        self.label_len = label_len
+        self.c_out = c_out
         self.attn = attn
         self.output_attention = output_attention
 
@@ -63,16 +68,19 @@ class Informer(nn.Module):
         # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
         # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
         #self.projection = nn.Linear(d_model, c_out, bias=True)
-        self.projection = nn.Sequential(
-            nn.Conv1d(72, 36, kernel_size=5), nn.Tanh(),  # 核大小为5
-            nn.AvgPool1d(2),  # 进行最大池化
-            nn.Conv1d(36, 72, kernel_size=5), nn.Tanh(),  # 核大小为5
-            nn.MaxPool1d(2),  # 进行最大池化
-            # nn.Flatten(),  #平展操作，使得通道维度最大，其他维度降维1
-            nn.Linear(125, 60), nn.Tanh(),  # 120输出的全连接层
-            nn.Linear(60, 30), nn.Tanh(),  # 60输出的全连接层
-            nn.Linear(30, 15), nn.Tanh(),   # 30输出的全连接层
-            nn.Linear(15, c_out), nn.Tanh())  # 1输出的全连接层
+        #--------------------------------------------------------------------#
+        # LeNet()框架
+#         self.projection = nn.Sequential(
+#             nn.Conv1d(72, 36, kernel_size=5), nn.Tanh(),  # 核大小为5
+#             nn.AvgPool1d(2),  # 进行最大池化
+#             nn.Conv1d(36, 72, kernel_size=5), nn.Tanh(),  # 核大小为5
+#             nn.MaxPool1d(2),  # 进行最大池化
+#             # nn.Flatten(),  #平展操作，使得通道维度最大，其他维度降维1
+#             nn.Linear(125, 60), nn.Tanh(),  # 120输出的全连接层
+#             nn.Linear(60, 30), nn.Tanh(),  # 60输出的全连接层
+#             nn.Linear(30, 15), nn.Tanh(),   # 30输出的全连接层
+#             nn.Linear(15, c_out), nn.Tanh())  # 1输出的全连接层
+#------------------------------------------------------------------------------#
         
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
@@ -81,12 +89,20 @@ class Informer(nn.Module):
 
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
+        #--------------------------------------------#
+        # 实现ResNet50模型
+        model = ResNet50(in_channels=(self.label_len+self.pred_len),classes=self.c_out).cuda()
+        dec_out = model(dec_out)
+        # dec_out = [32,2048,d_model/32(ResNet50有16个块，就是16*2=32)],
+        #--------------------------------------------#
         #dec_out = self.projection(dec_out)
         #LogSoftmax = nn.LogSoftmax(dim=2)
         #dec_out = LogSoftmax(dec_out)
-        for layer in self.projection:
-            dec_out = layer(dec_out)
-        
+        #-----------------------------------------------#
+        # 循环该LeNet()模型
+#         for layer in self.projection:
+#             dec_out = layer(dec_out)
+        #-----------------------------------------------#
         # dec_out = self.end_conv1(dec_out)
         # dec_out = self.end_conv2(dec_out.transpose(2,1)).transpose(1,2)
         if self.output_attention:
